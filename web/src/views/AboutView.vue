2fc65185f73440c415f929aaf0b72b27a0dffc4d
lvcastro <lucasvizzottodecastro@gmail.com>
2024-05-01 15:59:04 -0300

    small aesthetic adjustments

--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="bg-lightyellow mx-0 flex-grow-1 text-center">
-    <div class="container px-3 py-5">
+  <div class="bg-lightyellow mx-0 flex-grow-1">
+    <div class="container lato my-3 text-center">
       <h1 class="dark-green fs-1">Sobre nós</h1>
 
-      <h2 class="green-2 fs-3 my-4">Contexto</h2>
+      <h2 class="green-2 fs-3 mt-4 mb-2">Contexto</h2>
 
-      <p class="container dark-grey">
-        Projeto elaborado nas disciplinas de Introdução ao Desenvolvimento Web e Engenharia de Software do ICMC (USP São
-        Carlos) no período de 2024.1. Seu objetivo principal é facilitar a identificação dos principais problemas que impactam
-        a locomoção e a segurança nas ruas de São Carlos, buscando tornar sua manutenção mais eficiente, tendo em vista a
-        melhoria da qualidade de vida dos habitantes da cidade.
+      <p class="container dark-grey w-50 open-sans">
+        Este é um projeto elaborado nas disciplinas de Introdução ao Desenvolvimento Web e Engenharia de Software do
+        ICMC (USP São Carlos) no período de 2024.1. Seu objetivo principal é facilitar a identificação dos principais
+        problemas que impactam a locomoção e a segurança nas ruas de São Carlos, buscando tornar sua manutenção mais
+        eficiente, tendo em vista a melhoria da qualidade de vida dos habitantes da cidade.
       </p>
 
-      <h2 class="green-2 fs-3 my-4">Desenvolvedores:</h2>
-      <ul class="dark-grey">
+      <h2 class="green-2 fs-3 mt-4 mb-2">Desenvolvedores:</h2>
+      <ul class="dark-grey open-sans">
         <li>Antônio Medrado</li>
         <li>Diego Cabral</li>
         <li>Henrico Lazuroz</li>
--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -4,14 +4,12 @@ import MapComponent from '../components/MapComponent.vue'
 
 <template>
   <div class="bg-lightyellow mx-0 flex-grow-1">
-    <div class="container px-3 py-5">
-      <h1 class="dark-green fs-1 text-center mb-4">Denuncie um problema</h1>
+    <div class="container mt-3 mb-3 lato">
+      <h1 class="dark-green fs-1 text-center mb-2">Denuncie um problema</h1>
       <div class="card card-green">
         <div class="card-body card-green">
           <div class="mb-3">
-            <label for="report-subject-type-select" class="form-label text-white"
-              >Tipo do problema</label
-            >
+            <label for="report-subject-type-select" class="form-label text-white">Tipo do problema</label>
             <select class="form-select custom-form-field" id="report-subject-type-select">
               <option selected disabled hidden>Selecione uma opção</option>
               <option value="1">Buraco</option>
@@ -22,30 +20,17 @@ import MapComponent from '../components/MapComponent.vue'
           </div>
 
           <div class="mb-3">
-            <label for="report-email-input" class="form-label text-white"
-              >E-mail para receber atualizações (opcional)</label
-            >
-            <input
-              type="email"
-              class="form-control custom-form-field"
-              id="report-email-input"
-              aria-describedby="emailHelp"
-            />
+            <label for="report-email-input" class="form-label text-white">E-mail para receber atualizações (opcional)</label>
+            <input type="email" class="form-control custom-form-field" id="report-email-input" aria-describedby="emailHelp"/>
             <div id="emailHelp" class="form-text text-white">
               Nós nunca compartilharemos seu email com ninguém.
             </div>
           </div>
 
-          <p class="text-white text-center">Qual a localização do problema?</p>
-          <div class="my-3 d-flex justify-content-center">
-            <input
-              type="text"
-              class="form-control custom-form-field"
-              id="report-address-input"
-              v-model="address"
-              placeholder="Digite o endereço ou marque-o no mapa"
-              style="max-width: 500px"
-            />
+          <label for="report-address-input" class="container text-center text-white mb-0">Qual a localização do problema?</label>
+          <div class="mt-1 mb-3 d-flex justify-content-center">
+            <input type="text" class="form-control custom-form-field" id="report-address-input" v-model="address"
+                   placeholder="Digite o endereço ou marque-o no mapa" style="max-width: 500px"/>
           </div>
           <div class="container mb-3 d-flex justify-content-center">
             <MapComponent style="height: 300px; max-width: 500px; border-radius: 20px" />
