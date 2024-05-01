860f48118b5e23b110145cb11c9825ed3bdfb3ab
pliduino <liduinopedro@gmail.com>
2024-04-30 22:10:16 -0300

    removed unused style

--- web/src/components/MapComponent.vue
+++ web/src/components/MapComponent.vue
@@ -3,14 +3,8 @@ import { VMap, VMapOsmTileLayer, VMapZoomControl } from 'vue-map-ui'
 </script>
 
 <template>
-  <VMap id="map">
+  <VMap>
     <VMapOsmTileLayer />
     <VMapZoomControl />
   </VMap>
 </template>
-
-<style scoped>
-#map {
-  height: 100%;
-}
-</style>
