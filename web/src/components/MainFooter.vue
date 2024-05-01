b10d3d9959afd768bd72d482be98529d03ca573e
pliduino <liduinopedro@gmail.com>
2024-04-30 21:25:45 -0300

    Footer now on the bottom of the page

--- web/src/components/MainFooter.vue
+++ web/src/components/MainFooter.vue
@@ -1,3 +1,13 @@
 <template>
-  <footer>Blablabla</footer>
+  <footer>Footer</footer>
 </template>
+
+<style scoped>
+footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 60px;
+  background: #39878b;
+}
+</style>
