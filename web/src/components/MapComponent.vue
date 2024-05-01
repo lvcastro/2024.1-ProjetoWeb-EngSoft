44f7bd27a8502ebb7996a31da99e7e37e415ea44
pliduino <liduinopedro@gmail.com>
2024-04-30 22:20:14 -0300

    centralized map to SC

--- web/src/components/MapComponent.vue
+++ web/src/components/MapComponent.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { VMap, VMapOsmTileLayer, VMapZoomControl } from 'vue-map-ui'
+import { VMap, VMapOsmTileLayer, VMapZoomControl, VMapMarker } from 'vue-map-ui'
 </script>
 
 <template>
-  <VMap>
+  <VMap :center="[-22.0175, -47.89083]" :zoom="13">
     <VMapOsmTileLayer />
     <VMapZoomControl />
+    <VMapMarker :latlng="[-22.0175, -47.9]" />
   </VMap>
 </template>
