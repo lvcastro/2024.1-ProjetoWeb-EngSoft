ac1829a31bc2b968ce4d1a4a705625f5dfdfa742
Henrico Lazuroz <hlazuroz@uolinc.com>
2024-05-02 15:30:35 -0300

    Adicionando conteudo footer

--- web/src/components/MainFooter.vue
+++ web/src/components/MainFooter.vue
@@ -1,15 +1,18 @@
 <template>
-  <footer>Footer</footer>
+  <footer>
+    Alertaqui! / Av. Trab. São Carlense, 400 - Centro, São Carlos - SP, 13566-590 / <a href="https://www.saocarlos.sp.gov.br/"> fale com a gente </a> 
+  </footer>
 </template>
 
 <style scoped>
 footer {
   width: 100%;
-  height: 60px;
+  max-height: 100px;
   background: #39878b;
   position: relative;
   left: 0;
   bottom: 0;
   border-top: 10px solid #ece8d7;
+  text-align: center;
 }
 </style>
