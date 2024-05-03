0fd4ffce6b679ab6a0de6c74aba99750ae3ad272
lvcastro <lucasvizzottodecastro@gmail.com>
2024-05-02 22:08:59 -0300

    changes in TableView and small adjustment in AboutView "context" text

--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="bg-lightyellow mx-0 flex-grow-1">
     <div class="container lato my-3 text-center">
-      <h1 class="dark-green fs-1">Sobre nós</h1>
+      <h1 class="dark-green fs-1">Sobre o projeto</h1>
 
       <h2 class="green-2 fs-3 mt-4 mb-2">Contexto</h2>
 
       <p class="container dark-grey w-50 open-sans">
         Este é um projeto elaborado nas disciplinas de Introdução ao Desenvolvimento Web e Engenharia de Software do
-        ICMC (USP São Carlos) no período de 2024.1. Seu objetivo principal é facilitar a identificação dos principais
+        ICMC (USP São Carlos) no período de 2024.1. Nosso objetivo principal é facilitar a identificação dos principais
         problemas que impactam a locomoção e a segurança nas ruas de São Carlos, buscando tornar sua manutenção mais
-        eficiente, tendo em vista a melhoria da qualidade de vida dos habitantes da cidade.
+        eficiente, e assim trazendo uma melhora na qualidade de vida dos habitantes da cidade.
       </p>
 
       <h2 class="green-2 fs-3 mt-4 mb-2">Desenvolvedores</h2>
--- web/src/views/TableView.vue
+++ web/src/views/TableView.vue
@@ -1,61 +1,65 @@
 <template>
-    <div class="bg-lightyellow dark-green">
-        <div class="row px-2 py-4 attributes">  
-            <div class="col-md-4">
-                <p>Nome</p>
+    <div class="bg-lightyellow dark-green lato">
+        <h1 class="text-center my-3">Gerenciar administradores</h1>
+        <div class="container border border-2 border-black rounded">
+            <div class="row px-2 py-4 attributes">  
+                <div class="col-md-4">
+                    Nome
+                </div>
+                <div class="col-md-4">
+                    Email
+                </div>
+                <div class="col-md-3">
+                    CPF
+                </div>
+                <div class="col-md-1">
+                    <button type="button" data-bs-toggle="modal" data-bs-target="#modalAdicionar"><span class="material-symbols-outlined">add_circle</span></button>
+                </div>
             </div>
-            <div class="col-md-4">
-                <p>Email</p>
+            
+            <div class="row p-2">   
+                <div class="col-md-4">
+                    <p>Geraldo Fernandes da Silva</p>
+                </div>
+                <div class="col-md-4">
+                    <p>geraldofs234@gmail.com</p>
+                </div>
+                <div class="col-md-3">
+                    <p>961.836.290-65</p>
+                </div>
+                <div class="col-md-1">
+                    <button type="button"><span class="material-symbols-outlined">edit</span></button>
+                </div>
             </div>
-            <div class="col-md-3">
-                <p>CPF</p>
+            <div class="row p-2">   
+                <div class="col-md-4">
+                    <p>Arnaldo Gonçalves Siqueira</p>
+                </div>
+                <div class="col-md-4">
+                    <p>arnaldinhosiqueira55@gmail.com</p>
+                </div>
+                <div class="col-md-3">
+                    <p>432.808.980-30</p>
+                </div>
+                <div class="col-md-1">
+                    <button type="button"><span class="material-symbols-outlined">edit</span></button>
+                </div>
             </div>
-            <div class="col-md-1">
+            <div class="row p-2">   
+                <div class="col-md-4">
+                    <p>Vinícius Ferreira de Moraes</p>
+                </div>
+                <div class="col-md-4">
+                    <p>viniferreiramoraes@gmail.com</p>
+                </div>
+                <div class="col-md-3">
+                    <p>688.773.130-48</p>
+                </div>
+                <div class="col-md-1">
+                    <button type="button"><span class="material-symbols-outlined">edit</span></button>
+                </div>
             </div>
         </div>
-        
-        <div class="row p-2">   
-            <div class="col-md-4">
-                <p>Geraldo Fernandes da Silva</p>
-            </div>
-            <div class="col-md-4">
-                <p>geraldofs234@gmail.com</p>
-            </div>
-            <div class="col-md-3">
-                <p>961.836.290-65</p>
-            </div>
-            <div class="col-md-1">
-                <span class="material-symbols-outlined">edit</span>
-            </div>
-        </div>
-        <div class="row p-2">   
-            <div class="col-md-4">
-                <p>Arnaldo Gonçalves Siqueira</p>
-            </div>
-            <div class="col-md-4">
-                <p>arnaldinhosiqueira55@gmail.com</p>
-            </div>
-            <div class="col-md-3">
-                <p>432.808.980-30</p>
-            </div>
-            <div class="col-md-1">
-                <span class="material-symbols-outlined">edit</span>
-            </div>
-        </div>
-        <div class="row p-2">   
-            <div class="col-md-4">
-                <p>Vinícius Ferreira de Moraes</p>
-            </div>
-            <div class="col-md-4">
-                <p>viniferreiramoraes@gmail.com</p>
-            </div>
-            <div class="col-md-3">
-                <p>688.773.130-48</p>
-            </div>
-            <div class="col-md-1">
-                <span class="material-symbols-outlined">edit</span>
-            </div>
-        </div> 
     </div>
     
 </template>
