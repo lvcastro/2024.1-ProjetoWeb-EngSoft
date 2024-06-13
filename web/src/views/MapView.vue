667a906e4d26569be00230021b307d87a4ea2b4a
Antônio Medrado <apmedrado@usp.br>
2024-06-13 17:24:55 -0300

    Testing mapview gathering pins from backend

--- web/src/views/MapView.vue
+++ web/src/views/MapView.vue
@@ -1,18 +1,35 @@
 <script setup lang="ts">
-import MapComponent from '../components/MapComponent.vue'
-import SidebarComponent from '../components/SideBarComponent.vue'
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import MapComponent from '../components/MapComponent.vue';
+import SidebarComponent from '../components/SideBarComponent.vue';
+
+const markers = ref([]);
+
+// Função para buscar os marcadores do backend
+onMounted(async () => {
+  try {
+    const response = await axios.get('http://localhost:3000/reports');
+    if (Array.isArray(response.data)) {
+      markers.value = response.data.map(item => [item.latitude, item.longitude]);
+    } else {
+      console.error('Os dados recebidos do backend não são do tipo array:', response.data);
+    }
+  } catch (error) {
+    console.error('Erro ao buscar os marcadores do backend:', error);
+  }
+});
 </script>
 
 <template>
   <div class="bg-lightyellow">
     <div class="container-fluid lato">
-      <div class="row ">
+      <div class="row">
         <div class="col-md-3">
-            <SidebarComponent />
+          <SidebarComponent />
         </div>
-        
         <div class="col-md-9 d-block m-0">
-          <MapComponent />
+          <MapComponent :markers="markers" class="map-component" />
         </div>
       </div>
     </div>
