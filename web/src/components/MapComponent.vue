8e5bb8b002aef55687e4cce8d45d2db92d9cf38d
Henrico Lazuroz <hicolazu22@hotmail.com>
2024-06-23 11:19:24 -0300

    Adicionando icones personalizados para os marcadores

--- web/src/components/MapComponent.vue
+++ web/src/components/MapComponent.vue
@@ -7,6 +7,10 @@ import {
   VMapMarker,
   VMapAttributionControl,
 } from 'vue-map-ui'
+import tree from '../assets/tree.png';
+import stop from '../assets/stop.png';
+import lamp from '../assets/lamp.png';
+import hole from '../assets/hole.png';
 
 // Definindo os eventos que o componente pode emitir
 const emit = defineEmits(['map-click'])
@@ -23,6 +27,31 @@ const handleMapClick = (event) => {
   const { latlng } = event
   emit('map-click', { lat: latlng.lat, lng: latlng.lng })
 }
+
+const getIcon = (problem) => {
+  let iconUrl;
+  switch (problem) {
+    case 'arvore-caida  ':
+      iconUrl = tree;
+      break;
+    case 'falta-de-sinalizacao':
+      iconUrl = stop;
+      break;
+    case 'falta-de-iluminacao':
+      iconUrl = lamp;
+      break;
+    case 'buraco':
+      iconUrl = hole;
+      break;
+    default:
+      iconUrl = tree; // Ícone padrão, caso nenhum problema corresponda
+  }
+  return L.icon({
+    iconUrl: iconUrl,
+    iconSize: [30, 65]
+  });
+}
+
 </script>
 
 <template>
@@ -32,6 +61,6 @@ const handleMapClick = (event) => {
     <VMapScaleControl />
     <VMapAttributionControl position="hbottomcenter" />
     <!-- Renderiza os marcadores com base na lista de coordenadas fornecida -->
-    <VMapMarker v-for="(marker, index) in markers" :key="index" :latlng="marker.coordinates" />
+    <VMapMarker v-for="(marker, index) in markers" :key="index" :latlng="marker.coordinates" :icon="getIcon(marker.type)"/>
   </VMap>
 </template> 
